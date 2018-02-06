035bc16d87c59d2c4ceddc309b2815fb841437b1
Linar Abzaltdinov <abzaltdinov@gmail.com>
2018-02-07 01:03:53 +0300

    Added SimpleCalculatorTest class with some tests

--- hw01/src/ru/sberbank/homework/Abzaltdinov/Assert.java
+++ hw01/src/ru/sberbank/homework/Abzaltdinov/Assert.java
@@ -12,4 +12,10 @@ public class Assert {
             throw new AssertException(message);
         }
     }
+
+    public static void assertEquals(String message, String expected, String actual) throws AssertException {
+        if (!expected.equals(actual)) {
+            throw new AssertException(message);
+        }
+    }
 }
