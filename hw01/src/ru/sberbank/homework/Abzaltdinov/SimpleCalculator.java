3db7d9b285fc92abf6248b888d63b26228c54df2
Linar Abzaltdinov <abzaltdinov@gmail.com>
2018-02-06 22:16:28 +0300

    Added DivideByZeroException

--- hw01/src/ru/sberbank/homework/Abzaltdinov/SimpleCalculator.java
+++ hw01/src/ru/sberbank/homework/Abzaltdinov/SimpleCalculator.java
@@ -1,38 +1,48 @@
 package ru.sberbank.homework.Abzaltdinov;
 
 /**
- * Simple calculator with +,-,*,/ operation with int and double
+ * Simple calculator with +,-,*,/ operation with int firstnd double
  */
 public class SimpleCalculator {
-    public int sum(int a, int b) {
-        return a+b;
+    public int sum(int first, int second) {
+        return first+second;
     }
 
-    public int sub(int a, int b) {
-        return a-b;
+    public int sub(int first, int second) {
+        return first-second;
     }
 
-    public int mul(int a, int b) {
-        return a*b;
+    public int mul(int first, int second) {
+        return first*second;
     }
 
-    public int div(int a, int b) {
-        return a/b;
+    public int div(int first, int second) {
+        if (second == 0)
+            throw new DivideByZeroException();
+        return first/second;
     }
 
-    public double sum(double a, double b) {
-        return a+b;
+    public double sum(double first, double second) {
+        return first+second;
     }
 
-    public double sub(double a, double b) {
-        return a-b;
+    public double sub(double first, double second) {
+        return first-second;
     }
 
-    public double mul(double a, double b) {
-        return a*b;
+    public double mul(double first, double second) {
+        return first*second;
     }
 
-    public double div(double a, double b) {
-        return a/b;
+    public double div(double first, double second) {
+        if (second == 0)
+            throw new DivideByZeroException();
+        return first/second;
+    }
+    
+    class DivideByZeroException extends RuntimeException {
+        DivideByZeroException() {
+            super("Divided by zero!");
+        }
     }
 }
