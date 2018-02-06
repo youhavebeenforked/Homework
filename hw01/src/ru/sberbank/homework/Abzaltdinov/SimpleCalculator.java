8a020d570bb30a507f2cfe589f58e5e72e4a6a41
Linar Abzaltdinov <abzaltdinov@gmail.com>
2018-02-06 19:43:51 +0300

    Create SimpleCalculator class

---
+++ hw01/src/ru/sberbank/homework/Abzaltdinov/SimpleCalculator.java
@@ -0,0 +1,38 @@
+package ru.sberbank.homework.Abzaltdinov;
+
+/**
+ * Simple calculator with +,-,*,/ operation with int and double
+ */
+public class SimpleCalculator {
+    public int sum(int a, int b) {
+        return a+b;
+    }
+
+    public int sub(int a, int b) {
+        return a-b;
+    }
+
+    public int mul(int a, int b) {
+        return a*b;
+    }
+
+    public int div(int a, int b) {
+        return a/b;
+    }
+
+    public double sum(double a, double b) {
+        return a+b;
+    }
+
+    public double sub(double a, double b) {
+        return a-b;
+    }
+
+    public double mul(double a, double b) {
+        return a*b;
+    }
+
+    public double div(double a, double b) {
+        return a/b;
+    }
+}
