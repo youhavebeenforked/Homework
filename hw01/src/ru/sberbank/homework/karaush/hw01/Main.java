90c8b887b03bf12ed730bae132c9eaf77df1bd68
vladlen <vladlenkaraush@gmail.com>
2018-02-07 00:01:40 +0300

    Поменял с дженериков на примитивы, поменял способ сравнения даблов, добавил тесткейс, поправил стиль.

--- hw01/src/ru/sberbank/homework/karaush/hw01/Main.java
+++ hw01/src/ru/sberbank/homework/karaush/hw01/Main.java
@@ -1,12 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
-
-
     public static void main(String[] args) {
 
         SimpleCalculatorTests tests = new SimpleCalculatorTests();
-
         tests.runAllTests();
     }
 
