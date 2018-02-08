26b5ff1fce787b06832166ec2e4ce51cabbbfe1c
Ivan Maruev <ivshturm@mail.ru>
2018-02-08 11:38:48 +0300

    fixed

--- hw01/src/ru/sberbank/homework/Maruev/src/SimpleCalculator.java
+++ hw01/src/ru/sberbank/homework/Maruev/src/SimpleCalculator.java
@@ -11,31 +11,31 @@ public class SimpleCalculator {
         return one + two;
     }
 
-    public int sub(int one, int two) {
+    public int substract(int one, int two) {
         return one - two;
     }
 
-    public double sub(double one, double two) {
+    public double substract(double one, double two) {
         return one - two;
     }
 
-    public int mult(int one, int two) {
+    public int multiply(int one, int two) {
         return one * two;
     }
 
-    public double mult(double one, double two) {
+    public double multiply(double one, double two) {
         return one * two;
     }
 
-    public int div(int one, int two) {
+    public int division(int one, int two) {
         if (two == 0) {
             throw new AssertExpectedError("Exception zero");
         }
         return one / two;
     }
 
-    public double div(double one, double two) {
-        if (two == 0.0) {
+    public double division(double one, double two) {
+        if (two == 0) {
             throw new AssertExpectedError("Exception zero");
         }
         return one / two;
