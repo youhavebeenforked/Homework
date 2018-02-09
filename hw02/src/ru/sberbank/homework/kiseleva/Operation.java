a1e17bbf08f36d333634ab709cf67778c5eb6c70
Ekaterina Kiseleva <e.kiseleva@ro.ru>
2018-02-09 15:51:39 +0300

    hw 02

---
+++ hw02/src/ru/sberbank/homework/kiseleva/Operation.java
@@ -0,0 +1,17 @@
+package ru.sberbank.homework.kiseleva;
+
+/**
+ * класс-перечисление операций
+ */
+public enum Operation {
+    ADDITION('+'),
+    SUBTRACTION('-'),
+    MULTIPLICATION('*'),
+    DIVISION('/');
+
+    private final char operation;
+
+    Operation(char operation) {
+        this.operation = operation;
+    }
+}
