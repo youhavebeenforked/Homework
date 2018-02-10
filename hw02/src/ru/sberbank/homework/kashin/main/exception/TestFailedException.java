bcfd8c751b5e4cd460eaf8b260370df35ec11d92
kadmser <kadmser@gmail.com>
2018-02-10 13:04:01 +0300

    to gradle

--- hw02/src/ru/sberbank/homework/kashin/main/exception/TestFailedException.java
+++
@@ -1,7 +0,0 @@
-package ru.sberbank.homework.kashin.main.exception;
-
-public class TestFailedException extends RuntimeException {
-    public TestFailedException(String message) {
-        super(message);
-    }
-}
