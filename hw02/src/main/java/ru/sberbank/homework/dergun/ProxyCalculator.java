c246953bbcde251ccce3955ccd10fbc14f688b2f
Kroshik <magi_renn@mail.ru>
2018-02-12 01:17:49 +0300

    #2.2 Added support: underscore & long. In the case of a non-integer result, the last zeros are removed

--- hw02/src/main/java/ru/sberbank/homework/dergun/ProxyCalculator.java
+++ hw02/src/main/java/ru/sberbank/homework/dergun/ProxyCalculator.java
@@ -1,6 +1,8 @@
 package ru.sberbank.homework.dergun;
 
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ProxyCalculator implements Calculator {
     private static final double DBL_EPSILON = 1e-10;
@@ -23,7 +25,6 @@ public class ProxyCalculator implements Calculator {
         double doubleA;
         int i = 0;
         if (firstScan) {
-            firstScan = false;
             if (split.length != 3) {
                 return "error > wrong expression";
             }
@@ -33,6 +34,7 @@ public class ProxyCalculator implements Calculator {
                 return "error > " + split[i];
             }
             i++;
+            firstScan = false;
         } else {
             if (split.length != 2) {
                 return "error > wrong expression";
@@ -41,6 +43,7 @@ public class ProxyCalculator implements Calculator {
         }
         Operation operation;
         try {
+
             if(split[i].length() != 1) {
                 return "error > " + split[i];
             }
@@ -64,11 +67,26 @@ public class ProxyCalculator implements Calculator {
         if (Math.abs(result - (long) result) <= DBL_EPSILON) {
             return Long.toString((long) result);
         }
-        return String.format(Locale.ROOT, "%.2f", result);
+        String res = String.format(Locale.ROOT, "%.2f", result);
+        if(res.charAt(res.length() - 1) == '0') {
+            res = res.substring(0, res.length() - 1);
+            if(res.charAt(res.length() - 1) == '0') {
+                res = res.substring(0, res.length() - 2);
+            }
+        }
+        return res;
     }
 
     private double parseDouble(String s) {
         try {
+            Pattern pattern = Pattern.compile("^([+]|-)?(0x|0b|0|#)?([0-9]+[_]?[0-9]+)*[0-9]*");
+            Matcher matcher = pattern.matcher(s);
+            if(matcher.matches())
+                s = s.replaceAll("[_]", "");
+            pattern = Pattern.compile("([+]|-)?(0x|0b|0|#)?[0-9]*((l|L)?)$");
+            matcher = pattern.matcher(s);
+            if(matcher.matches())
+                s = s.replaceAll("l|L", "");
             if (s.length() > 2 && s.charAt(0) == '0' && s.charAt(1) == 'b') {
                 return Integer.parseInt(s.substring(2), 2);
             } else {
