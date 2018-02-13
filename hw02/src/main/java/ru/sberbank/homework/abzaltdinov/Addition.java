5edaf46577b23bb271b66c38688e8a4657a8eb7c
Linar Abzaltdinov <abzaltdinov@gmail.com>
2018-02-13 09:46:18 +0300

    Added Abstract class MyOperation with 4 implementations

---
+++ hw02/src/main/java/ru/sberbank/homework/abzaltdinov/Addition.java
@@ -0,0 +1,9 @@
+package ru.sberbank.homework.abzaltdinov;
+
+public class Addition extends MyOperation {
+
+    @Override
+    public double calculate(double first, double second) {
+        return first + second;
+    }
+}
