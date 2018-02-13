b21b5352ad63aad54ba67819eae481868f94454e
Kroshik <magi_renn@mail.ru>
2018-02-13 23:47:51 +0300

    #3 hw2 fix problem: unused variable, try with resources, access modifiers

--- hw03/src/main/java/ru/sberbank/homework/dergun/hw2/UrlConnect.java
+++ hw03/src/main/java/ru/sberbank/homework/dergun/hw2/UrlConnect.java
@@ -1,18 +1,16 @@
 package ru.sberbank.homework.dergun.hw2;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Scanner;
 
 public class UrlConnect {
     private URL url;
 
-    void start() {
+    public void start() {
         while (true) {
             String url = urlRead();
             if (readPage(url)) {
@@ -21,37 +19,32 @@ public class UrlConnect {
         }
     }
 
-    boolean readPage(String url) {
+    private boolean readPage(String url) {
         if (!urlValidator(url)) {
             return false;
         }
-        try {
-            URLConnection conn = this.url.openConnection();
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openConnection().getInputStream()))){
             String inputLine;
             while ((inputLine = br.readLine()) != null) {
                 System.out.println(inputLine);
             }
-            br.close();
             System.out.println("Done");
 
-        } catch (IOException ignored) {
+        } catch (Exception ignored) {
             System.err.println("Error");
         }
         return true;
     }
 
-    String urlRead() {
+    private String urlRead() {
         System.out.println("Enter the URL:");
         Scanner in = new Scanner(System.in);
-        String url = in.next();
-        return url;
+        return in.next();
     }
 
-    boolean urlValidator(String url) {
+    private boolean urlValidator(String url) {
         try {
-            this.url = new URL(url);
-            this.url.toURI();
+            new URL(url).toURI();
             return true;
         } catch (URISyntaxException | MalformedURLException e) {
             System.out.println("Can't install connection. Reply.");
