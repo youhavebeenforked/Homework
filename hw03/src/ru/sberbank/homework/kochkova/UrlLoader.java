e3d0c7ab13ccedb87908af879b699ac1fb8a0390
Cheshirrrrrr <kochkova@rain.ifmo.ru>
2018-02-18 18:20:23 +0300

    add hw03-2

---
+++ hw03/src/ru/sberbank/homework/kochkova/UrlLoader.java
@@ -0,0 +1,39 @@
+package ru.sberbank.homework.kochkova;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+public class UrlLoader {
+
+
+    private void readPage(String url) throws IOException {
+        URL page = new URL(url);
+        URLConnection connection = page.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine = in.readLine();
+        while (inputLine != null) {
+            System.out.println(inputLine);
+            inputLine = in.readLine();
+        }
+        in.close();
+    }
+
+    public void loadPage() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (true) {
+            try {
+                readPage(input);
+                break;
+            } catch (IOException e) {
+                System.out.println("Something went wrong, try to repeat your input: ");
+                input = scanner.nextLine();
+            }
+        }
+        scanner.close();
+    }
+}
