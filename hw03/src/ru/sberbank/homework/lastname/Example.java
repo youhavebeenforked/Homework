c9e4d024d32088d2499f1bc8eae1f589416a8d5b
vladlen <vladlenkaraush@gmail.com>
2018-02-19 13:15:44 +0300

    03.Karaush

--- hw03/src/ru/sberbank/homework/lastname/Example.java
+++ hw03/src/ru/sberbank/homework/lastname/Example.java
@@ -1,7 +1,14 @@
 package ru.sberbank.homework.lastname;
 
+import java.util.Scanner;
+
 public class Example {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Terminal terminal = new MyTerminal();
+        Scanner scanner = new Scanner(System.in);
+
+        Client client = new Client(terminal, scanner);
+        client.interact();
     }
 }
