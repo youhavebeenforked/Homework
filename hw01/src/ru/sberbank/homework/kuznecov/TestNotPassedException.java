418974ba2daa61567077d977bd8401eeb7f65ca5
Yaroslav <34842216+kydwork@users.noreply.github.com>
2018-02-19 11:51:41 +0300

    Delete TestNotPassedException.java

--- hw01/src/ru/sberbank/homework/kuznecov/TestNotPassedException.java
+++
@@ -1,9 +0,0 @@
-package ru.sberbank.homework.kuznecov;
-
-public class TestNotPassedException extends RuntimeException {
-
-    public TestNotPassedException(String message) {
-        super(message);
-    }
-
-}
