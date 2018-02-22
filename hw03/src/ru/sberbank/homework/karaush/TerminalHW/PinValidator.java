986cafb87cf090c033d6244a3b2740f6868a613f
vladlen <vladlenkaraush@gmail.com>
2018-02-22 17:20:59 +0300

    03-2. URlReader 1. Поменял название однобуквенной переменной 2. Поменял new Date.getTime() на System.currentTimeMillis() 3. Сделал статическими константы

--- hw03/src/ru/sberbank/homework/karaush/TerminalHW/PinValidator.java
+++ hw03/src/ru/sberbank/homework/karaush/TerminalHW/PinValidator.java
@@ -4,8 +4,8 @@ import java.util.Date;
 
 public class PinValidator {
 
-    private final int MAX_ATTEMPS_BEFORE_BLOCK = 3;
-    private final int BLOCK_DURATION_IN_MS = 5000;
+    private static final int MAX_ATTEMPTS_BEFORE_BLOCK = 3;
+    private static final int BLOCK_DURATION_IN_MS = 5000;
     private final String pin;
     private int attemptsCounter;
     private boolean accessGranted;
@@ -22,14 +22,14 @@ public class PinValidator {
         validateFormat(pin);
 
         if (new Date().getTime() < unblockTime) {
-            throw new IllegalAccessException(Long.toString(unblockTime - new Date().getTime()));
+            throw new IllegalAccessException(Long.toString(unblockTime - System.currentTimeMillis()));
         }
 
         attemptsCounter++;
-        if (attemptsCounter > MAX_ATTEMPS_BEFORE_BLOCK) {
+        if (attemptsCounter > MAX_ATTEMPTS_BEFORE_BLOCK) {
             attemptsCounter = 0;
             unblockTime = new Date().getTime() + BLOCK_DURATION_IN_MS;
-            throw new IllegalAccessException(Long.toString(unblockTime - new Date().getTime()));
+            throw new IllegalAccessException(Long.toString(unblockTime - System.currentTimeMillis()));
         }
 
         accessGranted = pin.equals(this.pin);
