2ff50764460a062b9e5a9fc7708828d938dfe9ff
Drozdov Gleb <224989@niuitmo.ru>
2018-02-26 15:52:50 +0300

    delete unnecessary files

--- hw01/src/ru/sberbank/homework/abzaltdinov/DivisionByZeroException.java
+++
@@ -1,7 +0,0 @@
-package ru.sberbank.homework.abzaltdinov;
-
-public class DivisionByZeroException extends RuntimeException {
-    DivisionByZeroException() {
-        super("Divided by zero!");
-    }
-}
