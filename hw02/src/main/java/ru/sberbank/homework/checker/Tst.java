b8f9e2b2ef527ac999bdbe2f731a50c657506ff6
Ivan_Maruev <32998948+ivshturm@users.noreply.github.com>
2018-03-03 22:27:12 +0300

    Delete Tst.java

--- hw02/src/main/java/ru/sberbank/homework/checker/Tst.java
+++
@@ -1,9 +0,0 @@
-package ru.sberbank.homework.checker;
-
-public class Tst {
-    public static void main(String[] args) {
-        long i = 0b1111111_11111111_11111111_11111111_11111111_11111111_11111111_11111111L + 2;
-        long a = Long.parseLong("111111111111111111111111111111111111111111111111111111111111111", 2);
-    }
-
-}
