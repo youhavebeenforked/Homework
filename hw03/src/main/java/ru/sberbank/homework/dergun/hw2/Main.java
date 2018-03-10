fef48a146f8637b28f20b88a1a2bb9a35212aa51
Ilya Solovyov <solovyov41@gmail.com>
2018-03-10 19:59:18 +0300

    Удаление лишнего

--- hw03/src/main/java/ru/sberbank/homework/dergun/hw2/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.sberbank.homework.dergun.hw2;
-
-public class Main {
-    public static void main(String[] args) {
-        UrlConnect urlConnect = new UrlConnect();
-        urlConnect.start();
-    }
-}
