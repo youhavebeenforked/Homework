10d02a710786f9aa7b03260d14086eef7f3faaeb
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2018-03-12 11:58:06 +0300

    Revert "hw05_2_NoClassDefFoundError"
    This reverts commit a28f0f0582b2986686b0d09ae0d076e9a200fa9f.

--- hw05/src/main/java/ru/sberbank/jschool/homework/bobrov/caesar/CreateCryptoClass.java
+++ hw05/src/main/java/ru/sberbank/jschool/homework/bobrov/caesar/CreateCryptoClass.java
@@ -1,21 +1,7 @@
 package ru.sberbank.jschool.homework.bobrov.caesar;
 
-import java.io.*;
-
 public class CreateCryptoClass {
+    void createCaesar(String path, int offset) {
 
-    void createCaesar(String in, String out, int offset) {
-        try (FileInputStream input = new FileInputStream(in);
-             FileOutputStream output = new FileOutputStream(out);
-        ) {
-            int ch;
-            while ((ch = input.read()) != -1) {
-                output.write((char) (ch + offset));
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
