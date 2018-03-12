bd29973d43a1bc964fa121be5dfc71251eb5fa58
Dmitriy Bobrov <bobrov.dmitriy@gmail.com>
2018-03-12 11:58:35 +0300

    Revert "hw05_2_begin"
    This reverts commit 7541fa83765332bb77d30c3aa382b1ed8a3d069a.

--- hw05/src/main/java/ru/sberbank/jschool/homework/bobrov/caesar/CreateCryptoClass.java
+++
@@ -1,7 +0,0 @@
-package ru.sberbank.jschool.homework.bobrov.caesar;
-
-public class CreateCryptoClass {
-    void createCaesar(String path, int offset) {
-
-    }
-}
--- hw05/src/main/java/ru/sberbank/jschool/homework/bobrov/caesar/EncryptedClassLoader.java
+++ hw05/src/main/java/ru/sberbank/jschool/homework/bobrov/caesar/EncryptedClassLoader.java
@@ -38,29 +38,27 @@ public class EncryptedClassLoader extends ClassLoader {
     }
 
     private byte[] fetchClassFromFS(String path) throws FileNotFoundException, IOException {
-//        InputStream is = new FileInputStream(new File(path));
-//        long length = new File(path).length();
-//
-//        byte[] bytes = new byte[(int) length];
-//
-//        int offset = 0;
-//        int numRead = 0;
-//        while (offset < bytes.length
-//                && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
-//            offset += numRead;
-//        }
-//
-//        // Ensure all the bytes have been read in
-//        if (offset < bytes.length) {
-//            throw new IOException("Could not completely read file " + path);
-//        }
-//
-//        // Close the input stream and return bytes
-//        is.close();
-//        return bytes;
-        byte[] result = new byte[0];
-        
-        return result;
+        InputStream is = new FileInputStream(new File(path));
+        long length = new File(path).length();
+
+        byte[] bytes = new byte[(int) length];
+
+        int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length
+                && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+            offset += numRead;
+        }
+
+        // Ensure all the bytes have been read in
+        if (offset < bytes.length) {
+            throw new IOException("Could not completely read file " + path);
+        }
+
+        // Close the input stream and return bytes
+        is.close();
+        return bytes;
+
 
     }
 
